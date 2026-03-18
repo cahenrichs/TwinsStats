bb81076f9af0f21cf46b155f33177cd28578aa15
cahenrichs <cahenrichs@gmail.com>
2026-03-18 21:15:00 +0000

    make the tests for FindPlayerByName

--- internal/mlb/clients_test.go
+++ internal/mlb/clients_test.go
@@ -1,1 +1,31 @@
 package mlb
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestFindPlayerByName(t *testing.T) {
+	//set up fake server
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path == "/teams/142/roster" {
+			w.WriteHeader(http.StatusOK)
+			fmt.Fprintln(w, `{
+			 roster: [
+				{
+					person: {
+						id: 12345,
+						fullName: "Buxton"
+					}
+				}
+			 ]
+			}`)
+		} else {
+			w.WriteHeader(http.StatusNotFound)
+		}
+
+	}))
+	defer server.Close()
+}
