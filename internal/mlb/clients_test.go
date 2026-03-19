76f1fc33f61986cfe0b7df4ba7876b816e2968dd
cahenrichs <cahenrichs@gmail.com>
2026-03-19 21:43:03 +0000

    Updated the current FindPlayerByName test

--- internal/mlb/clients_test.go
+++ internal/mlb/clients_test.go
@@ -13,14 +13,19 @@ func TestFindPlayerByName(t *testing.T) {
 		if r.URL.Path == "/teams/142/roster" {
 			w.WriteHeader(http.StatusOK)
 			fmt.Fprintln(w, `{
-			 roster: [
-				{
-					person: {
-						id: 12345,
-						fullName: "Buxton"
+				"roster": [
+					{
+						"person": {
+							"id": 12345,
+							"fullName": "Byron Buxton"
+						},
+						"jerseyNumber": "25",
+						"position": {
+							"code": "8",
+							"name": "Outfielder"
+						}
 					}
-				}
-			 ]
+				]
 			}`)
 		} else {
 			w.WriteHeader(http.StatusNotFound)
@@ -28,4 +33,25 @@ func TestFindPlayerByName(t *testing.T) {
 
 	}))
 	defer server.Close()
+
+	client := NewClient()
+	client.baseURL = server.URL + "/"
+	client.cache = t.TempDir()
+
+	t.Run("Player found", func(t *testing.T) {
+		player, err := client.FindPlayerByName(142, "Buxton")
+		if err != nil {
+			t.Fatalf("Expected no error, got %v", err)
+		}
+		if player.Person.FullName != "Byron Buxton" {
+			t.Errorf("Expecting to get Buxton, got %s", player.Person.FullName)
+		}
+	})
+
+	t.Run("Player not found", func(t *testing.T) {
+		_, err := client.FindPlayerByName(142, "Nonexistent Player")
+		if err == nil {
+			t.Fatal("Expected error, got nil")
+		}
+	})
 }
